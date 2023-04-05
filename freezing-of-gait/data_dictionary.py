8847704d0a64649adbc992b5bef538030f2e03cc
Utkarsh Anant <uanant007@gmail.com>
2023-04-05 23:09:36 +0530

    firest commit

---
+++ freezing-of-gait/data_dictionary.py
@@ -0,0 +1,14 @@
+data_dict = {
+    'time-of-sample': 'Time of sample (ms)',
+    'ank-f-acc': 'Ankle (shank) Fwd Acc (mg)',
+    'ank-v-acc': 'Ankle (shank) Ver Acc (mg)',
+    'ank-l-acc': 'Ankle (shank) Lat Acc (mg)',
+    'upleg-f-acc': 'Upper leg (thigh) Fwd Acc (mg)',
+    'upleg-v-acc': 'Upper leg (thigh) Ver Acc (mg)',
+    'upleg-l-acc': 'Upper leg (thigh) Lat Acc (mg)',
+    'trnk-f-acc': 'Trunk Fwd Acc (mg)',
+    'trnk-v-acc': 'Trunk Ver Acc (mg)',
+    'trnk-l-acc': 'Trunk Lat Acc (mg)',
+    'annot': 'Annotation',
+    'user': 'User'
+}
